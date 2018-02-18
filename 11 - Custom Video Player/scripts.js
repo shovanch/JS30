4411c9b655b79f2e627578591f9c74ace7d8bf72
Shovan Chatterjee <shovanc.web@gmail.com>
2018-02-19 01:12:58 +0530

    Done Module 11

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -2,7 +2,7 @@
 const player = document.querySelector(".player");
 const video = player.querySelector(".viewer");
 const progress = player.querySelector(".progress");
-const progressBar = player.querySelector(".progress_filled");
+const progressBar = player.querySelector(".progress__filled");
 const toggle = player.querySelector(".toggle");
 const skipButtons = player.querySelectorAll("[data-skip]");
 const ranges = player.querySelectorAll(".player__slider");
@@ -27,17 +27,45 @@ function skip() {
   video.currentTime += parseFloat(this.dataset.skip);
 }
 
+// Handle volume and playback speed
 function handleRangeUpdate() {
   video[this.name] = this.value;
+}
+
+// Handle video progress
+function handleProgress() {
+  let percent = video.currentTime / video.duration;
+  percent *= 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+// Scrub the video progress
+function scrubProgress(e) {
+  let scrubTime = e.offsetX / progress.offsetWidth;
+  scrubTime *= video.duration;
+  video.currentTime = scrubTime;
 }
 
 /* Hook up the event listener */
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
 video.addEventListener("pause", updateButton);
+video.addEventListener("timeupdate", handleProgress);
 
 toggle.addEventListener("click", togglePlay);
 skipButtons.forEach(button => button.addEventListener("click", skip));
 
 ranges.forEach(range => range.addEventListener("change", handleRangeUpdate));
 ranges.forEach(range => range.addEventListener("mousemove", handleRangeUpdate));
+
+// Making scrub progress on mousedown
+let mousedown = false;
+progress.addEventListener("click", scrubProgress);
+// If mouse is down, then only update the scrub progress
+progress.addEventListener("mousemove", e => mousedown && scrubProgress(e));
+progress.addEventListener("mousedown", () => (mousedown = true));
+progress.addEventListener("mouseup", () => {
+  mousedown = false;
+});
+
+/* CHALLENGE: ADD FULLSCREEN FUNCTIONALITY */
